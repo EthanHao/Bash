32c7addce77d433c2253271755e214a8929290f3
Weihua Hao <hao.weihua@qq.com>
2025-05-30 16:31:26 -0400

    Create 0530.mk

---
+++ linux/0530.mk
@@ -0,0 +1,32 @@
+1.  Command
+
+2,  How to connect commands
+   2.1  Pipeline
+   2.2  Sequencential
+   2.3  A && B  ( do both) 
+   2.4  A || B  ( do B if A failed)
+
+3,  moving around in File system
+  3.1  brace expansion  {1,2,3} , {A..Z..1}, {A, B, C}-{1..2}
+  3.2  wildcard in File system commands
+        ? ,  . , *
+  3.2  xargs
+  
+4, Variables
+  4.1 Enviroment variables
+  4.2 Shell variables
+  4.3 Local variables
+
+5, config your own Bash
+  5.1  config prompt
+  5.2  alias
+
+6, Bash function
+
+7, System info
+   7.1 uname
+   7.2 cpuinfo
+   7.3 free
+   7.4 df
+   7.5 ifconfig
+   7.6
